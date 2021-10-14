ef92f620b4b26b413a238dfe26d5fa5a52fe0096
DimasNandaHerlambang <55137751+DimasNandaHerlambang@users.noreply.github.com>
2021-10-15 00:44:14 +0700

    Vodop laravel 7
    Menyelsaikan Vidio laravel 7 tentang Post Model

--- Models/Post.php
+++ Models/Post.php
@@ -2,33 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-   private static $blog_posts =  [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Dimas Nanda Herlambang",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum eaque repellendus accusantium, beatae et laboriosam aperiam doloribus, obcaecati laudantium perspiciatis consectetur, voluptates eos animi dolorum repudiandae! Illo tenetur ex iste quis ut perspiciatis quae veniam, voluptates nemo dolore molestias repellat alias eos ullam rem dolorum, voluptate est at numquam maiores. In, maxime voluptatem quibusdam ratione nobis culpa porro sequi. Veritatis eius soluta aliquid eaque fugiat ducimus quam vero, eveniet asperiores doloribus in officia necessitatibus ut porro voluptas odit dolores."
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "irfan tea",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum eaque repellendus accusantium, beatae et laboriosam aperiam doloribus, obcaecati laudantium perspiciatis consectetur, voluptates eos animi dolorum repudiandae! Illo tenetur ex iste quis ut perspiciatis quae veniam, voluptates nemo dolore molestias repellat alias eos ullam rem dolorum, voluptate est at numquam maiores. In, maxime voluptatem quibusdam ratione nobis culpa porro sequi. Veritatis eius soluta aliquid eaque fugiat ducimus quam vero, eveniet asperiores doloribus in officia necessitatibus ut porro voluptas odit dolores." 
-    ]   
- ];
+    use HasFactory;
 
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
